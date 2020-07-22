3bafefff342188f1914b45229356b14a106ef5c7
DESKTOP-CPF5K67\yoobin <you3574@naver.com>
2020-07-22 17:12:33 +0900

    LocalTimeTest

--- src/test/java/com/jojoldu/boot/springboot/domain/posts/PostsRespositoryTest.java
+++ src/test/java/com/jojoldu/boot/springboot/domain/posts/PostsRespositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,5 +43,25 @@ public class PostsRespositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+
+        LocalDateTime now = LocalDateTime.of(2020,07,22,0,0,0);
+        postsRepository.save(Posts .builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts= postsList.get(0);
+
+        System.out.println(">>>>>>>> createDate=" + posts.getCreateDate()+ ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
